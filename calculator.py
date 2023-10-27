8fb98bbe5c9a49d6340e5a1df67ed97881acebf2
Erfanfallah <148434863+Erfanfallah@users.noreply.github.com>
2023-10-27 23:06:51 +0330

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,53 +1,60 @@
 import math
 
-op = input("enter op(+, /, *, -, sin, cos, tan, cot, factorial, sqrt):")
+while True:
 
-if op == "cos":
-    x = float(input("Enter x:"))
-    r = (math.cos(math.radians(x)))
+    op = input("enter op(+, /, *, -, sin, cos, tan, cot, factorial, sqrt)or exit:")
 
-if op == "cot":
-    x = float(input("Enter x:"))
-    z = (math.tan(math.radians(x)))
-    r = 1 / z
+    if op == "exit":
+        break
+    if op == "cos":
+        x = float(input("Enter x:"))
+        r = (math.cos(math.radians(x)))
 
-if op == "sin":
-    x = float(input("Enter x:"))
-    r = (math.sin(math.radians(x)))
+    if op == "cot":
+        x = float(input("Enter x:"))
+        z = (math.tan(math.radians(x)))
+        r = 1 / z
 
-if op == "sqrt":
-    x = float(input("Enter x:"))
-    r = (math.sqrt(x))
+    if op == "sin":
+        x = float(input("Enter x:"))
+        r = (math.sin(math.radians(x)))
 
-if op == "tan":
-    x = float(input("Enter x:"))
-    r = (math.tan(math.radians(x)))
+    if op == "sqrt":
+        x = float(input("Enter x:"))
+        if x < 0:
+            r = "Error"
+        else:
+            r = (math.sqrt(x))
 
-if op == "factorial":
-    x = int(input("Enter x:"))
-    r = (math.factorial(x))
+    if op == "tan":
+        x = float(input("Enter x:"))
+        r = (math.tan(math.radians(x)))
 
-if op == "/":
-    a = float(input("Enter a:"))
-    b = float(input("Enter b:"))
-    if b == 0:
-        r = "Error"
-    else:
-        r = a / b
+    if op == "factorial":
+        x = int(input("Enter x:"))
+        r = (math.factorial(x))
 
-if op == "*":
-    a = float(input("Enter a:"))
-    b = float(input("Enter b:"))
-    r = a * b
+    if op == "/":
+        a = float(input("Enter a:"))
+        b = float(input("Enter b:"))
+        if b == 0:
+            r = "Error"
+        else:
+            r = a / b
 
-if op == "+":
-    a = float(input("Enter a:"))
-    b = float(input("Enter b:"))
-    r = a + b
+    if op == "*":
+        a = float(input("Enter a:"))
+        b = float(input("Enter b:"))
+        r = a * b
 
-if op == "-":
-    a = float(input("Enter a:"))
-    b = float(input("Enter b:"))
-    r = a - b
+    if op == "+":
+        a = float(input("Enter a:"))
+        b = float(input("Enter b:"))
+        r = a + b
 
-print(r)
+    if op == "-":
+        a = float(input("Enter a:"))
+        b = float(input("Enter b:"))
+        r = a - b
+
+    print(r)
